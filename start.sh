5d9018e6d54b2121ab728d8b74f505744ed93c46
jae young <ki-kongboy@hanmail.net>
2014-11-07 17:30:50 +0900

    peerdb.json

---
+++ start.sh
@@ -0,0 +1,1 @@
+INSIGHT_NETWORK=livenet BITCOIND_USER=suwoncoinrpc BITCOIND_PASS=C1eJ8qzPYbBW54j8eQYbN9ZZuRGUe2B82d1Cd7ngMDFe INSIGHT_PUBLIC_PATH=public forever start node_modules/.bin/insight-bitcore-api
