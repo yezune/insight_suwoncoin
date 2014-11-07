aeafe95fd2dbdb297ce612ff1db0236da8140c32
jae young <ki-kongboy@hanmail.net>
2014-11-07 17:32:09 +0900

    stop.sh

---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+INSIGHT_NETWORK=livenet BITCOIND_USER=suwoncoinrpc BITCOIND_PASS=C1eJ8qzPYbBW54j8eQYbN9ZZuRGUe2B82d1Cd7ngMDFe INSIGHT_PUBLIC_PATH=public forever stop node_modules/.bin/insight-bitcore-api
+
+
+
+
